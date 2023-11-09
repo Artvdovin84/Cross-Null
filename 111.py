58040ea63f6a93fe58dc1f3fe577559f4363c68f
Artvdovin84 <149980874+Artvdovin84@users.noreply.github.com>
2023-11-09 22:21:39 +0800

    Add files via upload

--- 111.py
+++ 111.py
@@ -10,14 +10,31 @@ M[0][0]=''
 print('\n'.join('\t'.join(map(str, row)) for row in M))
 
 
-l = input('Введите координаты куда поставить "X"')
+#l = input('Введите координаты куда поставить "X" (пример 2 0)')
 
 def hod (pole,tip,x,y):
-    pole[x][y]=tip
+    if pole[x][y] == '-':
+        pole[x][y]=tip
+        return False
+    else:
+        print ('Поле занято, попробуй еще раз')
+        return True
+for i in range(1,10):
+    s = True
+    if i % 2 == 0:
+        while s:
+            l = input('Введите координаты куда поставить "О" (пример 2 0)')
+            x, y = int(l[0]) + 1, int(l[2]) + 1
+            s = hod(M, 'O', x, y)
+    else:
+        while s:
+            l = input('Введите координаты куда поставить "X" (пример 2 0)')
+            x, y = int(l[0]) + 1, int(l[2]) + 1
+            s = hod(M, 'X', x, y)
+    print('\n'.join('\t'.join(map(str, row)) for row in M))
 
 
-hod (M,'x',1,2)
 
-print('\n'.join('\t'.join(map(str, row)) for row in M))
+
 
 # print('\n'.join('\t'.join(map(str, row)) for row in M))
